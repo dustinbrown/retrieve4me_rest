27037eaa1e67e6aa53b0456e93363e35c1359f4e
dustinbrown <dustinjamesbrown@gmail.com>
2014-05-04 17:07:37 -0700

    updated spec file with class call and removed white space

--- spec/rest_spec.rb
+++ spec/rest_spec.rb
@@ -5,23 +5,24 @@ require 'json'
 
 
 #load config files
-config = MyConfig.new
+#config = MyConfig.new
 
-set :environment, :test
-
-def app
-  Sinatra::Application
-end
+#set :environment, :test
 
 describe "reverse service" do
   #enables get, post, etc..
   include Rack::Test::Methods
 
-  before :each do
-    #login to sinatra with http basic
-    basic_authorize config.attrs[:username], config.attrs[:password]
-  end
-  
+def app
+  Rest
+  #Sinatra::Application
+end
+
+#  before :each do
+#    #login to sinatra with http basic
+#    basic_authorize config.attrs[:username], config.attrs[:password]
+#  end
+
   #it ' should reverse the string you provide it' do
   #  reverse('dustin').should == 'nitsud'
   #end
@@ -30,24 +31,24 @@ describe "reverse service" do
     get '/complete'
     last_response.should be_ok
   end
-  
+
   it 'should return a json object which parses to an array' do
     get '/complete/names'
     body = JSON.parse(last_response.body)
     body.class.should == Array
   end
-  
+
   it 'should return a json object which parses to an array of hashes' do
     get '/complete/names'
     body = JSON.parse(last_response.body)
     body[0].class.should == Hash
   end
-  
-  
+
+
   #it 'should only expect a valid api key when loading restricted page' do
   #  get '/api=sIJb2djfqXkxEmctlG5aXD7ktnyZFtZL'
   #  las
   #  last_response.should be_ok
   #end
-  
+
 end
